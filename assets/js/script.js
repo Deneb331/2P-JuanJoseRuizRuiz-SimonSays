4e9cea5c9d845148a2059d8b4a4e9063f53be4ee
Deneb331 <juanjo1.seriousmail@gmail.com>
2022-10-05 20:07:06 +0000

    Fix button ilumination bug, game completed. Still need to fix emailjs bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,150 +1,179 @@
 //Wait for the DOM to finish loading before running the game
 //Get the button elements and add listeners to them
+let currentColorIndex = 0 ;
+let sequenceLength = 5;
 document.addEventListener('DOMContentLoaded', function() {
     let buttons = document.getElementsByTagName("button");
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "start") {
-                startGame(5);
+                startGame(sequenceLength);
             } else if(this.getAttribute("data-type") === "score-button") {
                 sendScore();
             } else {
                 if(gameStarted) {
                     if(this.getAttribute("data-type") === "control-green") {
-                        iluminateColorGreen(true);
+                        iluminateColor("green", true);
                     } else if(this.getAttribute("data-type") === "control-red") {
-                        iluminateColorRed(true);
+                        iluminateColor("red", true);
                     } else if(this.getAttribute("data-type") === "control-yellow") {
-                        iluminateColorYellow(true);
+                        iluminateColor("yellow", true);
                     } else if(this.getAttribute("data-type") === "control-blue") {
-                        iluminateColorBlue(true);
+                        iluminateColor("blue", true);
                     } else {
-                        alert("You received a wrong data type.")
+                        alert("You received a wrong data type.");
                     }
                 } else {
-                    console.log("Game didn't start")
+                    console.log("Game didn't start");
                 }
             }
-        })
+        });
     }
 });
 
-let playerList = [];
-let gameList = [];
-let gameStarted = false;
+let playerList = [];        //List of the buttons pressed by the player.
+let gameList = [];          //List of the random buttons the player has to press in order.
+let gameStarted = false;    //Boolean used to check when the player can press the buttons.
 
+/**
+ * Iluminates the color passed with a numeric index.
+ */
+function playColor(index) {
+    if(index == 0){
+        iluminateColor('green', false);
+    }
+    if(index == 1){
+       iluminateColor('red', false)
+    }
+    
+    if(index == 2){
+        iluminateColor('yellow', false);
+    }
+    
+    if(index == 3){
+        iluminateColor('blue', false);
+    }
+}
+
+/**
+ * When the start button is pressed, it generates a random sequence and initiates the button animation.
+ */
 function startGame(colorLimit){
     let colorList = [];
     let colorNum = colorLimit;
+    let intervalTime = 1500;
     for (let index = 0; index < colorNum; index++) {
         let random = Math.floor(Math.random() * (3 + 1));
         colorList.push(random);
     }
+    const playInterval = setInterval(function(){
+        if (currentColorIndex < colorList.length){
+            playColor(colorList[currentColorIndex]);
+            currentColorIndex = currentColorIndex + 1;
+        }
+        else{
+            clearInterval(playInterval);
+            currentColorIndex = 0;
+        }
+    }, intervalTime);
 
-    let timer = 3000;
+    gameList = colorList;
 
-    for (let index = 0; index < colorList.length; index++) {
-        if(colorList[index] === 0) {
-            setTimeout(iluminateColorGreen(false), timer);
-            console.log(colorList[index] + " Green");
-            timer += 3000;
-        } else if(colorList[index] === 1) {
-            setTimeout(iluminateColorRed(false), timer);
-            console.log(colorList[index] + " Red");
-            timer += 3000;
-        } else if(colorList[index] === 2) {
-            setTimeout(iluminateColorYellow(false), timer);
-            console.log(colorList[index] + " Yellow");
-            timer += 3000;
-        } else if(colorList[index] === 3) {
-            setTimeout(iluminateColorBlue(false), timer);
-            console.log(colorList[index] + " Blue");
-            timer += 3000;
+    setTimeout(function() {
+        gameStarted = true;
+    }, intervalTime * colorNum + 600);
+}
+
+/**
+ * Iluminates the button passed
+ */
+function iluminateColor(color, playerPressed){
+    const colorId = `btn--${color}`;
+    const colorClass = `${color}-active`;
+    document.getElementById(colorId).classList.add(colorClass);
+    
+    setTimeout(function(){
+        switchOffColor(color)
+    }, 500);
+    
+    if(playerPressed) {
+        if(color == "green") {
+            playerList.push(0);
+            checkPlayerList();
+        } else if(color == "red") {
+            playerList.push(1);
+            checkPlayerList();
+        } else if(color == "yellow") {
+            playerList.push(2);
+            checkPlayerList();
+        } else if(color == "blue") {
+            playerList.push(3);
+            checkPlayerList();
         }
     }
-    gameList = colorList;
-    gameStarted = true;
 }
 
-function iluminateColorGreen(playerPressed){
-    document.getElementById("btn--green").style.background = "url('../images/GreenSelected.png')";
-    setTimeout(backToNormal("btn--green", "url('../images/GreenSelected.png')"), 3);
-    if(playerPressed) {
-        playerList.push(0);
-        checkPlayerList();
-    }
+/**
+ * Switch off the button passed.
+ */
+function switchOffColor(color){
+    const colorId = `btn--${color}`;
+    const colorClass = `${color}-active`;
+    document.getElementById(colorId).classList.remove(colorClass);
 }
 
-function iluminateColorRed(playerPressed){
-    document.getElementById("btn--red").style.background = "url('../images/RedSelected.png')";
-    setTimeout(backToNormal("btn--red", "url('../images/RedSelected.png')"), 3);
-    if(playerPressed) {
-        playerList.push(1);
-        checkPlayerList();
-    }
-}
-
-function iluminateColorYellow(playerPressed){
-    document.getElementById("btn--yellow").style.background = "url('../images/YellowSelected.png')";
-    setTimeout(backToNormal("btn--yellow", "url('../images/YellowSelected.png')"), 3);
-    if(playerPressed) {
-        playerList.push(2);
-        checkPlayerList();
-    }
-}
-
-function iluminateColorBlue(playerPressed){
-    document.getElementById("btn--blue").style.background = "url('../images/BlueSelected.png')";
-    setTimeout(backToNormal("btn--blue", "url('../images/BlueSelected.png')"), 3);
-    if(playerPressed) {
-        playerList.push(3);
-        checkPlayerList();
-    }
-}
-
-function backToNormal(buttonType, buttonImage) {
-    document.getElementById(buttonType).style.background = buttonImage;
-}
-
+/**
+ * Checks if the game has finished and if the player introduced the correct sequence.
+ */
 function checkPlayerList() {
     if(playerList.length === gameList.length){
-        if(playerList === gameList){
+        const equal = (playerList, gameList) => JSON.stringify(playerList) === JSON.stringify(gameList);
+        if(equal(playerList, gameList)){
             alert("Good job");
             playerList = [];
             gameList = [];
             incrementScore();
             gameStarted = false;
         } else {
-            alert("Try again");
+            alert(playerList + gameList + "Try again");
             playerList = [];
             gameList = [];
             incrementWrongScore();
             gameStarted = false;
         } 
     } else {
-        console.log(playerList.length)
+        console.log(playerList.length);
     }
 }
 
+/**
+ * Increments the score in case the player gets the correct sequence.
+ */
 function incrementScore() {
     let oldScore = parseInt(document.getElementById("correct").innerText);
     document.getElementById("correct").innerText = ++oldScore;
 }
 
+/**
+ * Increments the wrong score in case the player gets the wrong sequence.
+ */
 function incrementWrongScore() {
     let oldScore = parseInt(document.getElementById("incorrect").innerText);
     document.getElementById("incorrect").innerText = ++oldScore;
 }
 
+/**
+ * Sends the score to the player's email specified in the form.
+ */
 function sendScore() {
     let params = {
         "to_name" : document.getElementById("full-name").innerHTML,
         "email" : document.getElementById("player-email").innerHTML,
         "message" : "You had " + document.getElementById("correct").innerHTML + "correct answers and " + document.getElementById("incorrect").innerHTML + "wrong answers."
-    }
+    };
 
     emailjs.send("service_20gl1ow","template_m97fktr", params).then(function(res){
-        alert("Your score has been sended.")
+        alert("Your score has been sended.");
     });
 
     document.getElementById("full-name").innerHTML = "";
